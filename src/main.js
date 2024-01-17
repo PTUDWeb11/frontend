03f445c72e43b85358b815bf118216eb8c28864d
20120582 <newacc2312@gmail.com>
2024-01-18 00:53:26 +0700

    Integrating VuesticUI (Treeshaking,  AG Grid excluded)

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import router from './router';
 import { createPinia } from 'pinia';
 import vue3GoogleLogin from 'vue3-google-login';
 import './assets/index.css';
+import { createVuesticEssential, VaButton } from 'vuestic-ui'
+import 'vuestic-ui/styles/essential.css'
+import 'vuestic-ui/styles/grid.css'
+import 'vuestic-ui/styles/reset.css'
+import 'vuestic-ui/styles/typography.css'
 
 const pinia = createPinia();
 const app = createApp(App);
@@ -16,4 +21,6 @@ app.use(vue3GoogleLogin, {
 		'462472722132-7dm0elokfg5pck4467h16q8mahv1njhi.apps.googleusercontent.com',
 });
 
+
+app.use(createVuesticEssential({ components: { VaButton } }));
 app.mount('#app');
