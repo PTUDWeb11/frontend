89d95e8603f230e0b9bb5afc6decebacc744f027
20120582 <newacc2312@gmail.com>
2024-01-18 16:50:47 +0700

    Add CatergoriesNav to HomePage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
-	<div class="home">
-		<HelloWorld msg="Welcome to Your Vue.js App" />
+	<div class="home p-10">
+		<CategoriesNav />
 	</div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import CategoriesNav from "@/components/CategoriesNav.vue";
 
 export default {
-	name: 'HomeView',
+	name: "HomeView",
 	components: {
-		HelloWorld,
+		CategoriesNav,
 	},
 };
 </script>
