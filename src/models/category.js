baf125612ec864971b8b3525790960bcb3907ea2
20120582 <newacc2312@gmail.com>
2024-01-18 16:47:28 +0700

    Update Category model with sub-categories field

--- src/models/category.js
+++ src/models/category.js
@@ -10,13 +10,15 @@ export default class Category {
 	 * @param {number|null} parent_id - The ID of the parent category. Null if no parent.
 	 * @param {string} name - The name of the category.
 	 * @param {string|null} image - The URL of the category image.
+	 * @param {Category[]} subCategories - The subcategories of the category.
 	 */
 
-	constructor(id, parent_id, name, image) {
+	constructor(id, parent_id, name, image, subCategories) {
 		this.id = id;
 		this.parent_id = parent_id;
 		this.name = name;
 		this.image = image;
+		this.subCategories = subCategories;
 	}
 	save() {
 		// TODO: save category to database
