2cac9b1e8e65af2ea82aa27de226f75e27a47292
20120582 <newacc2312@gmail.com>
2024-01-19 15:30:53 +0700

    Add NewYear banner

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,11 @@
 <template>
 	<div class="home p-10">
 		<CategoriesNav />
+		<img
+			src="images/happy_new_year_banner.jpg"
+			alt="Happy New Year Banner"
+			class="w-4/6 h-64 object-cover mx-auto my-3"
+		/>
 	</div>
 </template>
 
