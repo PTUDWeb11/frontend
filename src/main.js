feb700f5196a528650a9b06ab910aab40f4ad014
20120558 <luungocquang2002@gmail.com>
2024-01-19 17:59:49 +0700

    Adjusted vuestic-ui import and drafted admin layout

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 import vue3GoogleLogin from "vue3-google-login";
-import { createVuesticEssential, VaButton } from "vuestic-ui";
 import "vuestic-ui/styles/essential.css";
 import "vuestic-ui/styles/grid.css";
 import "vuestic-ui/styles/typography.css";
 import App from "./App.vue";
 import "./assets/index.css";
 import router from "./router";
+import { createVuestic } from "vuestic-ui";
 
 const pinia = createPinia();
 const app = createApp(App);
@@ -19,6 +19,5 @@ app.use(vue3GoogleLogin, {
 	clientId:
 		"462472722132-7dm0elokfg5pck4467h16q8mahv1njhi.apps.googleusercontent.com",
 });
-
-app.use(createVuesticEssential({ components: { VaButton } }));
+app.use(createVuestic())
 app.mount("#app");
