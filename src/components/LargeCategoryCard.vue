59b04c11a2810512b6f8ee29ea60549e7947200b
20120582 <newacc2312@gmail.com>
2024-01-28 00:34:06 +0700

    Implement PopularCategoriesSection component

---
+++ src/components/LargeCategoryCard.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="large-category-card">
+		<a href="#" class="flex flex-col justify-center px-3 py-1">
+			<div class="aspect-square w-[130px] h-[130px]">
+				<img
+					class="image h-full w-full object-scale-down"
+					:src="category.image"
+				/>
+			</div>
+
+			<span class="block mt-2 text-center font-semibold">
+				{{ category.name }}
+			</span>
+		</a>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "LargeCategoryCard",
+	components: {},
+	props: {
+		category: {
+			type: Object,
+			required: true,
+		},
+	},
+};
+</script>
+
+<style scoped>
+.image {
+	max-width: 100%;
+	max-height: 100%;
+}
+</style>
