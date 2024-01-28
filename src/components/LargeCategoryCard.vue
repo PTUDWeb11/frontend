df34e054ec7d2ff24e9b201d5c04de5aee0b6c8b
20120582 <newacc2312@gmail.com>
2024-01-29 02:02:32 +0700

    Fix displaying jpeg images in LargeCategoryCard

--- src/components/LargeCategoryCard.vue
+++ src/components/LargeCategoryCard.vue
@@ -2,13 +2,20 @@
 	<div class="large-category-card">
 		<a href="#" class="flex flex-col justify-center items-center px-3 py-1">
 			<div class="aspect-square w-[130px] h-[130px]">
-				<div class="svg-container h-full w-full object-scale-down">
+				<div v-if="isSvg" class="svg-container h-full w-full object-scale-down">
 					<object
 						:data="category.image"
 						type="image/svg+xml"
 						width="100%"
 						height="100%"
 					></object>
+				</div>
+				<div v-else class="flex items-center h-full">
+					<img
+						class="object-cover"
+						:src="category.image"
+						:alt="category.name"
+					/>
 				</div>
 			</div>
 
@@ -27,6 +34,11 @@ export default {
 		category: {
 			type: Object,
 			required: true,
+		},
+	},
+	computed: {
+		isSvg() {
+			return this.category.image.endsWith(".svg");
 		},
 	},
 };
